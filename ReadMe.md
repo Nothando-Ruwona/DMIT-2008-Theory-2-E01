1862404da6827261700c759b3fa478759cfbae51
Nothando-Ruwona <nruwona1@nait.ca>
2026-08-08 03:57:19 -0600

    added weather card with full testing and changed readme file name

--- ReadMe.md
+++ ReadMe.md
@@ -1,6 +1,6 @@
 # Theory 2
 
-> Student Name: **YOUR_NAME**
+> Student Name: Nothando Tapuwanashe Ruwona github:Nothando-Ruwona
 
 ![Website Screenshot](./docs/images/screenshot.png)
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "storage.ko-fi.com",
+      },
+      {
+        protocol: "https",
+        hostname: "openweathermap.org",
       },
     ],
   },
